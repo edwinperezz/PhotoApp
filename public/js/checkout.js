d2e09626e5319f9befed21bc9eb54f2f682d069a
Edwin Perez <edwinperez@edwins-mbp.home>
2019-07-04 17:50:55 -0700

    changes

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,3 +1,6 @@
-$("#buy-arrow").click(function(){
+$(".buy-arrow").click(function(){
 	$("#form-container").toggleClass("hidden");
+	$(".buy-arrow i").toggleClass("fa-times");
+	$(".buy-arrow i").toggleClass("fa-arrow-right");
+	$(".buy-arrow").toggleClass("clicked");
 });
