60f88288c4668dea28cdb553db059802c6f9a375
Edwin Perez <edwinperez@edwins-mbp.home>
2019-07-07 22:33:48 -0700

    Lots of new changes

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const defaultClient = squareConnect.ApiClient.instance;
 // Configure OAuth2 access token for authorization: oauth2
 const oauth2 = defaultClient.authentications['oauth2'];
 oauth2.accessToken = accessToken;   
+console.log(process.env.DATABASEURL);
 mongoose.connect(process.env.DATABASEURL, { 
   useNewUrlParser: true,
   useCreateIndex: true
