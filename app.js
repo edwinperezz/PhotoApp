43cdd248753e7f098858fcbac1125f8a99922996
Edwin Perez <edwinperez@edwins-mbp.home>
2019-07-08 05:35:24 -0700

    added ability to add photos

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var app = express();
 const bodyParser    = require('body-parser'),
-squareConnect = require('square-connect'),
-mongoose      = require("mongoose"),
-Photo         = require("./models/photo");
+      squareConnect = require('square-connect'),
+      mongoose      = require("mongoose"),
+      Photo         = require("./models/photo"),
+      multer        = require("multer");
+      
 // Set the Access Token and Location Id
 const accessToken = 'EAAAEP3TIhoeAeQ4m2_BXPeCiwDJ_08Ie6cZBBux5KAo1YvybHEGpUVjmQ1SqvBP';
 const locationId = 'CBASEFoNja9W744N5jVfaDj8pyogAQ';
@@ -12,7 +14,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname+"/public/uploads/imgs")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+var upload = multer({storage: storage});
 // Set Square Connect credentials
 const defaultClient = squareConnect.ApiClient.instance;
 
@@ -41,8 +51,9 @@ app.get("/", (req,res) =>{
 app.get("/new", (req,res) => {
   res.render("new");
 });
-app.post("/new", (req,res) => {
-  var newPhoto = {title: req.body.title, ID: req.body.id, image: req.body.image, description: req.body.description};
+app.post("/new", upload.single('image'), (req,res) => {
+  var imgURL = "/uploads/imgs/"+req.file.originalname;
+  var newPhoto = {title: req.body.title, ID: req.body.id, image: imgURL, description: req.body.description};
   Photo.create(newPhoto, function(err,photo){
     if(err){
       console.log(err);
